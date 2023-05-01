fa420452261a08dc569dbdf62838f4f955adf9cf
Evan North <evan.north@sas.com>
2023-05-01 11:46:31 -0400

    The Parser Class v2

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -5,36 +5,29 @@ import shutil
 
 class Parser:
 
-    extensions : List[str] = []
-
-    def __init__(self):
-        self.extensions = Parser.extensions
+    extensions: List[str] = []
 
     def valid_extension(self, extension):
-        if extension.type() in self.extensions:
-            return True
-        else:
-            return False
+        return extension in self.extensions
     
-    def parse(self, path:Path, source:Path, dest:Path):
+    def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
     
-    def read(self, path:Path):
-        with open(path, mode='rt', encoding='utf-8') as file:
+    def read(self, path: Path):
+        with open(path, "r") as file:
             return file.read()
     
     def write(self, path, dest, content, ext=".html"):
         full_path = dest / path.with_suffix(ext).name
-        with open(full_path, mode='wt', encoding='utf-8') as file:
+        with open(full_path, "w") as file:
             file.write(content)
     
     def copy(self, path, source, dest):
-        shutil.copy2(src=path, dst=(dest / path.relative_to(source)))
+        shutil.copy2(path, dest / path.relative_to(source))
     
 
 class ResourceParser(Parser):
     extensions = [".jpg", ".png", ".gif", ".css", ".html"]
 
-
-    def parse(self, path:Path, source:Path, dest:Path):
+    def parse(self, path: Path, source: Path, dest: Path):
         Parser.copy(self, path, source, dest)
