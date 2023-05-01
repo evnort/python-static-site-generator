a2f468a19c95f4922a984881b292ae4fdda4ea23
Evan North <evan.north@sas.com>
2023-05-01 10:15:34 -0400

    The Site Class v3

--- ssg/site.py
+++ ssg/site.py
@@ -6,11 +6,11 @@ class Site:
         self.source = Path(source)
         self.dest = Path(dest)
     
-    def create_dir(self, path : Path):
+    def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
     
-    def build(self, source, dest):
+    def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
